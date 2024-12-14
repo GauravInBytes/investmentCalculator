b48c2f7cf2804c11f70e61ec3f8715bb89927fb9
GauravInBytes <gode429@gmail.com>
2024-12-14 22:03:33 +0530

    Initialized and added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+const Header = () => {
+  return (
+    <header id="header">
+        <img src={logo} alt="logo" />
+        <h2>Investment Calculator</h2>
+    </header>
+  )
+};
+
+export default Header;
